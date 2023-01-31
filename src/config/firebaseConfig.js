0ba4604b4f3c257b01afca505f56cf873d11dbc8
shubham-991 <dohaiyashubham991@gmail.com>
2023-01-31 16:41:56 +0530

    first commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDxN_JLa9UXr2ghxa_MkvXitqOxtfBU3s4",
+  authDomain: "crypto-clock.firebaseapp.com",
+  projectId: "crypto-clock",
+  storageBucket: "crypto-clock.appspot.com",
+  messagingSenderId: "765679961906",
+  appId: "1:765679961906:web:da98e294e365703984b92b"
+};
+
+export default firebaseConfig;
